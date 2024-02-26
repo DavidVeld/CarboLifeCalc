180d89ad4aee18021eec7e16eaedd759a11b7091
David In't veld <davidveld@gmail.com>
2024-02-26 13:35:38 +0000

    ui_icon

--- CarboLifeUI/Properties/Resources.Designer.cs
+++ CarboLifeUI/Properties/Resources.Designer.cs
@@ -249,5 +249,25 @@ namespace CarboLifeUI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ui_table {
+            get {
+                object obj = ResourceManager.GetObject("ui_table", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ui_table16 {
+            get {
+                object obj = ResourceManager.GetObject("ui_table16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
